46825e0d441b2ad54c0cac2e1bbb876875e27296
Jasec <jasec_7@hotmail.com>
2025-08-21 02:38:58 -0700

    complete python p3 oraganizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Mr. Chocolate', 3, 'M', 'Black', 'Chill', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Rowdy', 8, 'M', 'Brown', 'Goofy', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Tabitha', 2, 'F', 'Brown', 'Shy', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Sergeant Brown', 6, 'M', 'Brown', 'Serious', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Melissa', 3, 'F', 'White', 'Chill', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Grinch', 10, 'M', 'Black', 'Bad', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Wendy', 5, 'M', 'White', 'Mysterios', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES (NULL, 1, 'M', 'Black', 'Chill', True);
